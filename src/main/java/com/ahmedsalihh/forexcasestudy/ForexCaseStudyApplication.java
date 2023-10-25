0f8cb01c20af0bbc448951526415fc0e361e29fa
ahmedsalihh <ahmedsalihh@gmail.com>
2023-10-26 00:21:26 +0300

    Initial Commit

---
+++ src/main/java/com/ahmedsalihh/forexcasestudy/ForexCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedsalihh.forexcasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexCaseStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmedsalihh/forexcasestudy/ForexCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedsalihh.forexcasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexCaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
